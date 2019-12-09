efeb057539be0a57496ea077732952662151255a
wafaa <wafaades1417@gmail.com>
2019-12-09 04:33:36 +0300

    Update sw.js
    fix the SW to work in offline mode

--- sw.js
+++ sw.js
@@ -1,7 +1,24 @@
-var CacheName = 'Restaurant offline cache';
+var CacheName = 'restaurant-cahce';
 
 let cacheAssests = [
     '/',
+    './restaurant.html',
+    './css/styles.css',
+    './data/restaurants.json',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './js/dbhelper.js',    
+
 ];
 // insalling the SW andd add the pages to the cache
 self.addEventListener('install', function (event) {
@@ -23,7 +40,7 @@ self.addEventListener('activate', function (event) {
         caches.keys().then(function (cacheNames) {
             return Promise.all(
                 cacheNames.filter(function (cacheName) {
-                    return cacheName.startsWith('Restaurant') &&
+                    return cacheName.startsWith('restaurant') &&
                         cacheName != CacheName;
                 }).map(function (cacheName) {
                     return caches.delete(cacheName);
