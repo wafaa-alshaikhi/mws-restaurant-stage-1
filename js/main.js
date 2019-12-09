a108a3a816e7631391da4c78a903f61c2b55cd11
wafaa <wafaades1417@gmail.com>
2019-12-09 04:02:09 +0300

    fix the udacity Reviewer Notes

--- js/main.js
+++ js/main.js
@@ -147,7 +147,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(image);
   
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
